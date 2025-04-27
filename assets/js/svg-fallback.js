b2c8d80f1d927139bd85c23a8eb405d09c081fe8
sen-llc <0y.kawakami@gmail.com>
2025-04-27 16:11:12 +0900

    Update svg-fallback.js

--- assets/js/svg-fallback.js
+++ assets/js/svg-fallback.js
@@ -1,10 +1,10 @@
 // SVGアイコンのパスを修正
-// 相対パスから絶対パスに変更
-
 document.addEventListener("DOMContentLoaded", () => {
   // SVGをサポートしているかどうかをチェック
   if (!!document.createElementNS && !!document.createElementNS("http://www.w3.org/2000/svg", "svg").createSVGRect) {
-    return // SVGサポートあり、何もしない
+    // SVGサポートあり、パスの修正のみ行う
+    fixSvgPaths()
+    return
   }
 
   // SVGをサポートしていない場合、フォールバック処理を実行
@@ -14,10 +14,28 @@ document.addEventListener("DOMContentLoaded", () => {
     var element = elements[i]
     var href = element.getAttributeNS("http://www.w3.org/1999/xlink", "href")
 
-    if (href && href.indexOf(".svg") > -1) {
+    if (href && href.indexOf("icon-set.svg") > -1) {
       // SVGアイコンのパスを修正
-      var newHref = href.replace(/^(.*?)assets\/icons\//, "/assets/icons/")
+      var newHref = href.replace(/^(.*?)assets\/icons\/icon-set.svg/, "/assets/icons/icon-set.svg")
       element.setAttributeNS("http://www.w3.org/1999/xlink", "href", newHref)
     }
   }
 })
+
+// SVGパスを修正する関数
+function fixSvgPaths() {
+  var elements = document.querySelectorAll("svg use")
+
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i]
+    var href = element.getAttributeNS("http://www.w3.org/1999/xlink", "href")
+
+    if (href && href.indexOf("icon-set.svg") > -1) {
+      // 相対パスを絶対パスに変換
+      if (!href.startsWith("/assets/")) {
+        var newHref = href.replace(/^(.*?)assets\/icons\/icon-set.svg/, "/assets/icons/icon-set.svg")
+        element.setAttributeNS("http://www.w3.org/1999/xlink", "href", newHref)
+      }
+    }
+  }
+}
